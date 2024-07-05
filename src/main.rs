830384f05a53cee69b60b335d6d990628b92d166
M K <mkpro118@gmail.com>
2024-07-05 00:30:35 -0500

    Refactor building docs to it's own function, accept CLAs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
 use std::fs;
 
-mod entity;
-mod token;
-mod parser;
 mod ast;
+mod entity;
 mod md_gen;
+mod parser;
+mod token;
 
-fn build_docs(
-    input_file: &str,
-    output_file: Option<&str>
-) -> Result<bool, &'static str> {
+fn build_docs(input_file: &str, output_file: Option<&str>) -> Result<(), String> {
+    let Ok(data) = fs::read_to_string(input_file) else {
+        return Err(format!("File '{}' not found", &input_file));
+    };
 
-}
-
-fn main() {
-    let filename = "";
-    let data = fs::read_to_string(filename).expect("File not found!");
     let tokens = token::Token::tokenize(&data);
-
     let parsed = parser::parse_tokens(&tokens);
     let ast = ast::AST::build_ast(&parsed);
     let md = md_gen::generate_md(&ast);
 
-    println!("{}", md);
+    match output_file {
+        Some(_file) => {}
+        None => println!("{}", md),
+    };
+
+    Ok(())
+}
+
+fn main() {
+    let filename = std::env::args().nth(1).expect("No arguments");
+    match build_docs(&filename, None) {
+        Ok(_) => println!("Done!"),
+        Err(msg) => println!("Failed! {}", msg),
+    }
 }
