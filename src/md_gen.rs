9c8db5b176f47b3196e06f96c1ee8ea0d1aaaf05
M K <mkpro118@gmail.com>
2024-07-05 14:48:14 -0500

    Basic markdown escapes to prettify output

--- src/md_gen.rs
+++ src/md_gen.rs
@@ -7,6 +7,30 @@ pub fn generate_md(ast: &AST) -> String {
         .join("\n\n")
 }
 
+fn replace_leading_whitespace(s: &str) -> String {
+    let (leading_whitespace, rest) = s.split_at(
+        s.chars()
+            .position(|c| !c.is_whitespace())
+            .unwrap_or(s.len()),
+    );
+
+    let nbsp_prefix = leading_whitespace
+        .chars()
+        .skip(1)
+        .map(|_| "&nbsp;")
+        .collect::<String>();
+
+    nbsp_prefix + rest
+}
+
+fn md_escape(content: &str) -> String {
+    content
+        .split("\n")
+        .map(replace_leading_whitespace)
+        .collect::<Vec<String>>()
+        .join("\n")
+}
+
 impl<'a> Node<'a> {
     fn md_gen_visit(&self) -> String {
         match self.get_value() {
@@ -29,7 +53,7 @@ impl<'a> NodeTypes<'a> {
         let NodeTypes::Enum(node) = self else { panic!("Wrong type") };
         let mut md = String::new();
         md.push_str(format!("## Enum `{}`\n\n", node.name).as_str());
-        md.push_str(format!("{}\n\n", comment).as_str());
+        md.push_str(format!("{}\n\n", md_escape(&comment)).as_str());
         md.push_str("**Variants**:\n");
 
         for variant in &node.variants {
@@ -43,7 +67,7 @@ impl<'a> NodeTypes<'a> {
         let NodeTypes::Function(node) = self else { panic!("Wrong type") };
         let mut md = String::new();
         md.push_str(format!("## Function `{}`\n\n", node.name).as_str());
-        md.push_str(format!("{}\n\n", comment).as_str());
+        md.push_str(format!("{}\n\n", md_escape(&comment)).as_str());
         md.push_str(format!("Return Type `{}`\n\n", node.return_type).as_str());
         md.push_str("**Parameters**:\n");
 
@@ -58,11 +82,11 @@ impl<'a> NodeTypes<'a> {
         let NodeTypes::Struct(node) = self else { panic!("Wrong type") };
         let mut md = String::new();
         md.push_str(format!("## Struct `{}`\n\n", node.name).as_str());
-        md.push_str(format!("{}\n\n", comment).as_str());
+        md.push_str(format!("{}\n\n", md_escape(&comment)).as_str());
         md.push_str("**Members**:\n");
 
-        for variant in &node.members {
-            md.push_str(format!("- `{}`\n", variant).as_str());
+        for member in &node.members {
+            md.push_str(format!("- `{}`\n", member).as_str());
         }
 
         md
